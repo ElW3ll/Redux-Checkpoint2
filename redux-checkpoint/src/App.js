63d9fe0fc836d510b3843230035b0d5c21fab74a
ElW3ll <wael.mhamdi@gmail.com>
2021-02-18 21:12:30 +0100

    first checkpoint didn't uplode

---
+++ redux-checkpoint/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import AddItem from './Components/AddItem';
+import ListItems from './Components/ListItems';
+
+function App() {
+  return (
+    <div className="App">
+     <AddItem />
+     <ListItems />
+    </div>
+  );
+}
+
+export default App;
+
